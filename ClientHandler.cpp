7eeb02ad081168fdaf206c7924922e36a540ce8e
Keeper <hd_keeper@mail.ru>
2019-04-04 12:30:17 +0300

    Update ClientHandler.cpp

--- ClientHandler.cpp
+++ ClientHandler.cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 
 ClientHandler::ClientHandler(int socket) {
-    this->socket = socket;
-    for (int i = 0; i < SEQ_COUNT; i++) {
-        cur[i] = start[i] = step[i] = 0;
-    }
+	this->socket = socket;
+	for (int i = 0; i < SEQ_COUNT; i++) {
+		cur[i] = start[i] = step[i] = 0;
+	}
 }
 
 ClientHandler::~ClientHandler() {
-    Socket::close(socket);
+	Socket::close(socket);
 }
 
 
@@ -26,99 +26,99 @@ void ClientHandler::main() {
 	string inputBuf;
 	static string prompt("> ");
 
-    try {
-        // Отправить приглашение
-        if (!Socket::ok(Socket::send(socket, prompt.c_str(), (int)prompt.size(), 0))) {
-            throw Socket::error("Network send error");
-        }
-        
-        while (true) {
-            // Получить данные
-            memset(recvBuf, 0, sizeof(recvBuf));
-            int received = Socket::recv(socket, recvBuf, CMD_MAX_LEN, 0);
-            if (received == 0) {
-                throw Socket::error("Connection closed");
-            }
-            else if (!Socket::ok(received)) {
-                throw Socket::error("Network recv error");
-            }
+	try {
+		// Отправить приглашение
+		if (!Socket::ok(Socket::send(socket, prompt.c_str(), (int)prompt.size(), 0))) {
+			throw Socket::error("Network send error");
+		}
 
-            // Ищем конец введенной команды
-            inputBuf.append(recvBuf);
-            size_t endPos = 0;
-            if ((endPos = inputBuf.find("\n")) != string::npos) {
-                string command(inputBuf.substr(0, endPos));
-                inputBuf.assign(inputBuf.substr(endPos + 1));
+		while (true) {
+			// Получить данные
+			memset(recvBuf, 0, sizeof(recvBuf));
+			int received = Socket::recv(socket, recvBuf, CMD_MAX_LEN, 0);
+			if (received == 0) {
+				throw Socket::error("Connection closed");
+			}
+			else if (!Socket::ok(received)) {
+				throw Socket::error("Network recv error");
+			}
 
-                // Обработать команду
-                executeCommand(command);
+			// Ищем конец введенной команды
+			inputBuf.append(recvBuf);
+			size_t endPos = 0;
+			if ((endPos = inputBuf.find("\n")) != string::npos) {
+				string command(inputBuf.substr(0, endPos));
+				inputBuf.assign(inputBuf.substr(endPos + 1));
 
-                // Отправить приглашение
-                if (!Socket::ok(Socket::send(socket, prompt.c_str(), (int)prompt.size(), 0))) {
-                    throw Socket::error("Network send error");
-                }
-            }
+				// Обработать команду
+				executeCommand(command);
 
-            this_thread::sleep_for(chrono::milliseconds(50));
-        }
-    }
-    catch (Socket::error &ex) {
+				// Отправить приглашение
+				if (!Socket::ok(Socket::send(socket, prompt.c_str(), (int)prompt.size(), 0))) {
+					throw Socket::error("Network send error");
+				}
+			}
+
+			this_thread::sleep_for(chrono::milliseconds(50));
+		}
+	}
+	catch (Socket::error &ex) {
 		// cout << ex.what() << endl;
-        delete this;
-        return;
-    }
+		delete this;
+		return;
+	}
 }
 
 
 bool ClientHandler::executeCommand(const string &cmd) {
-    static regex reSeqCmd("^seq(\\d) (\\d+) (\\d+)");
-    static regex reExportCmd("^export seq");
-    smatch m;
+	static regex reSeqCmd("^seq(\\d) (\\d+) (\\d+)");
+	static regex reExportCmd("^export seq");
+	smatch m;
 
-    if (regex_search(cmd, m, reSeqCmd)) {
-        return setupSequence(
-            stoi(m[1]),
-            (uint64_t) stoll(m[2]),
-            (uint64_t) stoll(m[3]));
-    }
+	if (regex_search(cmd, m, reSeqCmd)) {
+		return setupSequence(
+			stoi(m[1]),
+			(uint64_t)stoll(m[2]),
+			(uint64_t)stoll(m[3]));
+	}
 
-    if (regex_search(cmd, m, reExportCmd)) {
-        return exportSequence();
-    }
+	if (regex_search(cmd, m, reExportCmd)) {
+		return exportSequence();
+	}
 
-    return false;
+	return false;
 }
 
 // Здесь i от 1 до SEQ_COUNT
 bool ClientHandler::setupSequence(int idx, uint64_t start, uint64_t step) {
-    if (idx < 1 || idx > ClientHandler::SEQ_COUNT) {
-        return false; 
-    }
-    if (step == 0) {
-        return false;
-    }
+	if (idx < 1 || idx > ClientHandler::SEQ_COUNT) {
+		return false;
+	}
+	if (step == 0) {
+		return false;
+	}
 
-    auto i = idx - 1;
-    this->start[i] = start;
-    this->step[i] = step;
-    return true;
+	auto i = idx - 1;
+	this->start[i] = start;
+	this->step[i] = step;
+	return true;
 }
 
 bool ClientHandler::exportSequence() {
-    // Проверить, что хотя бы одна последовательность задана
+	// Проверить, что хотя бы одна последовательность задана
 	bool defined = false;
-    for (int i = 0; i < ClientHandler::SEQ_COUNT; i++) {
-        cur[i] = start[i];
+	for (int i = 0; i < ClientHandler::SEQ_COUNT; i++) {
+		cur[i] = start[i];
 		defined = defined || (start[i] && step[i]);
-    }
+	}
 	if (!defined) {
 		return false;
 	}
 
-    // Генерация последовательности
-    while (true) {
-        string sendBuf;
-        for (int i = 0; i < ClientHandler::SEQ_COUNT; i++) {
+	// Генерация последовательности
+	while (true) {
+		string sendBuf;
+		for (int i = 0; i < ClientHandler::SEQ_COUNT; i++) {
 			if (start[i] && step[i]) {
 				if (sendBuf.length()) {
 					sendBuf.append("\t");
@@ -131,16 +131,16 @@ bool ClientHandler::exportSequence() {
 					cur[i] += step[i];
 				}
 			}
-        }
+		}
 		sendBuf.append("\r\n");
 
-        // Отправить в сокет
-        if (!Socket::ok(Socket::send(socket, sendBuf.c_str(), (int)sendBuf.size(), 0))) {
-            throw Socket::error("Network send error");
-        }
-         
-        this_thread::sleep_for(chrono::milliseconds(50));
-    }
+		// Отправить в сокет
+		if (!Socket::ok(Socket::send(socket, sendBuf.c_str(), (int)sendBuf.size(), 0))) {
+			throw Socket::error("Network send error");
+		}
 
-    return true;
+		this_thread::sleep_for(chrono::milliseconds(50));
+	}
+
+	return true;
 }
